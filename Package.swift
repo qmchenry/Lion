cbe633dc946c56b3a716da5dcfc05721674ecc93
Quinn McHenry <quinn@jqm.us>
2017-07-25 12:30:50 -0400

    Update GenKit package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ import PackageDescription
 let package = Package(
     name: "lion",
     dependencies: [
-        .Package(url: "https://github.com/SmallPlanet/GenKit.git", Version(0, 1, 0, prereleaseIdentifiers: ["pre"], buildMetadataIdentifier: "2")),
+        .Package(url: "https://github.com/SmallPlanet/GenKit.git", Version(0, 1, 0, prereleaseIdentifiers: ["pre"], buildMetadataIdentifier: "3")),
     ]
 )
